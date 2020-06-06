8f6d2ba72553d8e2c9042be4df1654bebc19e9c6
Larrygf02 <raulgf_02@hotmail.com>
2020-06-06 14:11:37 -0500

    Add component bookList

---
+++ client/src/components/BookList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function BookList() {
+    return (
+        <diV>
+            <ul id="book-list">
+                <li>Book Name</li>
+            </ul>
+        </diV>
+    )
+}
+
+export default BookList;
