3424efbffd561c527ba88db4ff734d8318d0990c
Larrygf02 <raulgf_02@hotmail.com>
2020-06-06 14:26:04 -0500

    Graphql setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import BookList from './components/BookList';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+// set up (apollo)
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 function App() {
   return (
-    <div id="main">
-      <h1>Reading List</h1>
-      <BookList></BookList>
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Reading List</h1>
+        <BookList></BookList>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const getBooksQuery = gql`
+    {
+        books {
+            id
+            name
+        }
+    }
+`
 function BookList() {
+    const { loading, error, data } = useQuery(getBooksQuery) 
+    if (loading) return <p>Loading..</p>
+    if (error) return <p>Error..</p>
+    console.log(data)
     return (
-        <diV>
+        <div>
             <ul id="book-list">
                 <li>Book Name</li>
             </ul>
-        </diV>
+        </div>
     )
 }
 
