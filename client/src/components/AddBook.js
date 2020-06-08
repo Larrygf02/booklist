ffc8c56fb3e7461e34e2fde7cb67b1221f4e3f42
Larrygf02 <raulgf_02@hotmail.com>
2020-06-07 22:22:40 -0500

    Render data

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -15,6 +15,7 @@ function AddBook() {
         setName('')
         setGenre('')
         setAuthor('')
+        window.location.reload()
     }
     if (loading) return <p>Loading..</p>
     if (error) return <p>Error..</p>
